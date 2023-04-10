8a7f4ad092017880d3860dcd07927877738fd91c
Matthew Dirks <majadirks@gmail.com>
2023-04-09 20:26:29 -0700

    Wrote basic method for projectile motion

--- HercAndHippoLib/Library.fs
+++ HercAndHippoLib/Library.fs
@@ -1,11 +1,33 @@
 ﻿namespace HercAndHippoLib
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+open System
 
-    let exampleFunc (x:int) =
-        x * x + 2
+module Motion =
 
-    let exampleFunc2(x:double) =
-        x * x + (double)2
+    type Point = 
+        { 
+        X: int
+        Y: int
+        }
+
+        static member Default = { X = 0; Y = 0; }
+
+        member this.Move(xDelta, yDelta) = 
+            { 
+            X = this.X + xDelta
+            Y = this.Y + yDelta
+            }
+
+    let private ProjectileMotionX(startX: int, startVelocityX: int, time: float) =
+        int ((float startX) + (float startVelocityX) * time)
+
+    let private ProjectileMotionY(gravity: float, startY: int, startVelocityY: int, time: float) =
+        (-0.5 * gravity * time * time) + (float startVelocityY * time) + (float startY)
+
+    let ProjectileMotion(gravity: float, startLocation: Point, startVelocity: Point, time: float) =
+        { 
+        X = Convert.ToInt32(ProjectileMotionX(startLocation.X, startVelocity.X, time))
+        Y = Convert.ToInt32(ProjectileMotionY(gravity, startLocation.Y, startVelocity.Y, time))
+        }
+            
+        
--- HercAndHippoLib_Test/Tests.fs
+++ HercAndHippoLib_Test/Tests.fs
@@ -1,17 +1,33 @@
 namespace HercAndHippoLib_Test
-open HercAndHippoLib.Say
+open HercAndHippoLib.Motion
 
 open System
 open Microsoft.VisualStudio.TestTools.UnitTesting
+
+
 
 [<TestClass>]
 type TestClass () =
 
     [<TestMethod>]
-    member this.TestMethodPassing () =
-        Assert.IsTrue(true);
-
-    [<TestMethod>]
-    member this.DoStuffWithX () =
-        Assert.AreEqual(6, exampleFunc 2)
-        Assert.AreEqual(2.01, exampleFunc2 0.1) // Dangerous comparison of doubles
+    member this.PointProjectileMotion () =
+        // Arrange
+        let startPoint = Point.Default
+        let startVelocity = {Point.X = 2; Y = 20}
+        let expectedOutput = [
+            {Point.X = 0; Y = 0}; 
+            {Point.X = 2; Y = 18};
+            {Point.X = 4; Y = 30};
+            {Point.X = 6; Y = 38};
+            {Point.X = 8; Y = 40};
+            {Point.X = 10; Y = 38};
+            {Point.X = 12; Y = 30};
+            {Point.X = 14; Y = 18};
+            {Point.X = 16; Y = 0}]
+        // Act
+        let throwList =
+            [0 .. 8]
+            |> List.map (fun time -> ProjectileMotion(5, startPoint, startVelocity, time))
+        // Assert
+        let doesMatch = expectedOutput = throwList
+        Assert.IsTrue(doesMatch)
