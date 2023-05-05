5833de488b3c481e4bbec682948548b91ec5c3bb
Matthew Dirks <majadirks@gmail.com>
2023-05-04 23:31:40 -0700

    Minor refactoring

--- HercAndHippoConsole/Program.cs
+++ HercAndHippoConsole/Program.cs
@@ -8,28 +8,10 @@ while (keyInfo == default || keyInfo.KeyChar != 'q')
     Console.Clear();
     Player player = level.FindPlayer();
     // Display objects in level
-    foreach (IDisplayable displayable in level.Displayables.OrderBy(d => d.Color).ThenBy(d => d.Location.Row).ThenBy(d => d.Location.Col))
+    IEnumerable<IDisplayable> toDisplay = level.Displayables.OrderBy(d => d.Color).ThenBy(d => d.Location.Row).ThenBy(d => d.Location.Col);
+    foreach (IDisplayable displayable in toDisplay)
     {
-        Console.SetCursorPosition(displayable.Location.Col, displayable.Location.Row);
-        Console.ForegroundColor = displayable.Color switch
-        {
-            Color.Red => ConsoleColor.Red,
-            Color.Yellow => ConsoleColor.Yellow,
-            Color.Green => ConsoleColor.Green,
-            Color.Blue => ConsoleColor.Blue,
-            Color.Purple => ConsoleColor.Magenta,
-            Color.Black => ConsoleColor.Black,
-            Color.White => ConsoleColor.White,
-            _ => ConsoleColor.White
-        } ;
-        Console.Write(displayable switch
-        {
-            Wall _ => "█",
-            BreakableWall _ => "▓",
-            Door _ => "D",
-            Player _ => "☺",
-            _ => "?"
-        });
+       WriteDisplayable(displayable);
     }
 
     Console.SetCursorPosition(1, Console.BufferHeight - 2);
@@ -41,4 +23,34 @@ while (keyInfo == default || keyInfo.KeyChar != 'q')
     if (keyInfo.Key == ConsoleKey.UpArrow) level = level.WithPlayer(player.MoveUp());
     if (keyInfo.Key == ConsoleKey.DownArrow) level = level.WithPlayer(player.MoveDown());
 }
+
+void WriteDisplayable(IDisplayable displayable)
+{
+    Console.SetCursorPosition(displayable.Location.Col, displayable.Location.Row);
+    Console.ForegroundColor = GetColor(displayable);
+    Console.Write(DisplayString(displayable));
+}
+
+ConsoleColor GetColor(IDisplayable displayable)
+=> displayable.Color switch
+    {
+        Color.Red => ConsoleColor.Red,
+        Color.Yellow => ConsoleColor.Yellow,
+        Color.Green => ConsoleColor.Green,
+        Color.Blue => ConsoleColor.Blue,
+        Color.Purple => ConsoleColor.Magenta,
+        Color.Black => ConsoleColor.Black,
+        Color.White => ConsoleColor.White,
+        _ => ConsoleColor.White
+    };
+
+string DisplayString(IDisplayable displayable)
+    => displayable switch
+    {
+        Wall _ => "█",
+        BreakableWall _ => "▓",
+        Door _ => "D",
+        Player _ => "☺",
+        _ => "?"
+    };
 
