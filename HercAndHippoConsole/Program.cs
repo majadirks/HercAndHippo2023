a5a836091c7dbb947f04a68f3972d4a8b10ac376
Matthew Dirks <majadirks@gmail.com>
2023-05-06 10:50:34 -0700

    Replay history in reverse at the end

--- HercAndHippoConsole/Program.cs
+++ HercAndHippoConsole/Program.cs
@@ -4,17 +4,24 @@ using System.Diagnostics;
 const int REFRESH_RATE = 20;
 
 Stopwatch sw = new();
-Level curState = TestLevels.WallsLevel;
+
 ConsoleKeyInfo keyInfo = default;
 bool firstRender = true;
-sw.Start();
 
+// Just for kicks,  record the gameplay for playback at the end.
+// initialize history stack with 5 minutes' worth of history
+Stack<Level> history = new(1000 / REFRESH_RATE * 60 * 5);
+
+Level curState = TestLevels.WallsLevel;
+Level newState = curState;
+
+sw.Start();
 while (true)
 {
     while (sw.ElapsedMilliseconds < REFRESH_RATE);
     sw.Restart();
 
-    Level newState = curState.RefreshCyclables(keyInfo);
+    newState = curState.RefreshCyclables(keyInfo);
     if (keyInfo.KeyChar == 'q') break;
     keyInfo = default;
     RefreshDisplay(curState, newState, firstRender);
@@ -25,12 +32,25 @@ while (true)
         keyInfo = Console.ReadKey();  
 }
 
+// Play back the recording in reverse
+curState = history.Peek();
+newState = history.Peek();
+while (history.Any())
+{
+    while (sw.ElapsedMilliseconds < REFRESH_RATE / 5) ;
+    sw.Restart();
+    newState = history.Pop();
+    RefreshDisplay(curState, newState, forceRefresh: false);
+    curState = newState;
+}
+
 void RefreshDisplay(Level oldState, Level newState, bool forceRefresh)
 {
     if (forceRefresh) Console.Clear();
     if (!forceRefresh && newState.HasSameStateAs(oldState)) return;
     ClearOld(oldState, newState);
     ShowNew(oldState, newState, forceRefresh);
+    history.Push(newState);
 }
 
 void ShowNew(Level oldState, Level newState, bool forceRefresh)
