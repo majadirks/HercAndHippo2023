d175942f5c3ff9fbfa8312cc8d1c51e57a055635
Matthew Dirks <majadirks@gmail.com>
2023-05-06 22:50:08 -0700

    Unit test for NoReaction

--- HercAndHippoLibCsTest/Behaviors_Test.cs
+++ HercAndHippoLibCsTest/Behaviors_Test.cs
@@ -9,24 +9,36 @@ namespace HercAndHippoLibCsTest
         public void DieAndStopBullet_Test()
         {
             // Arrange
-
-            // P*B
             Bullet bullet = new((2, 1), Direction.East);
             BreakableWall breakableWall = new(ConsoleColor.Green, (3, 1));
             Level initialState = new(
                 Player: new((1, 1), Health: 100, AmmoCount: 5, Inventory: new()),
                 Displayables: new()  { bullet, breakableWall });
-
             Level expectedEndingState = initialState.Without(bullet).Without(breakableWall);
-
             // Act
-            Level actualEndingState = initialState
-                .RefreshCyclables(default) // bullet moves to same location as breakable wall
-                .RefreshCyclables(default);
-
+            Level actualEndingState = Behaviors.DieAndStopBullet(breakableWall, initialState, bullet);
             // Assert
             Assert.IsTrue(expectedEndingState.HasSameStateAs(actualEndingState));
+        }
 
+        [TestMethod]
+        public void NoReaction_Test()
+        {
+
+            // Arrange
+            Level level = new(Player: new((2, 3), Health: 100, AmmoCount: 5, Inventory: new()),
+                Displayables: new()
+                {
+                    new BreakableWall(ConsoleColor.Green, (1,2)),
+                    new BreakableWall(ConsoleColor.Green, (2,2)),
+                    new BreakableWall(ConsoleColor.Green, (3,2)),
+                    new BreakableWall(ConsoleColor.Green, (1,3)),
+                    new BreakableWall(ConsoleColor.Green, (3,3))
+                });
+            // Act
+            Level nextState = Behaviors.NoReaction(level);
+            // Assert
+            Assert.IsTrue(level.HasSameStateAs(nextState));
         }
     }
 }
