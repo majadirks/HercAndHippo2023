78d7573dcc6a399a2060025823bd506d6002bec5
Matthew Dirks <majadirks@gmail.com>
2023-05-14 09:56:15 -0700

    cosmetics

--- HercAndHippoConsole/Program.cs
+++ HercAndHippoConsole/Program.cs
@@ -6,40 +6,34 @@ const int REFRESH_FREQUENCY_HZ = 50;
 
 // Initialize data
 CycleTimer cycleTimer = new(frequencyHz: REFRESH_FREQUENCY_HZ);
-ConsoleKeyInfo keyInfo = default;
+ConsoleKeyInfo keyInfo;
 Level state = DemoLevels.WallsLevel;
 ScrollStatus scrollStatus = ScrollStatus.Default(state.Player.Location);
 BufferStats bufferStats = new(BufferSizeChanged: true, BufferWidth: Console.BufferWidth, BufferHeight: Console.BufferHeight);
 DisplayPlan displayPlan = new(state, scrollStatus, bufferStats);
 Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+// Initialize display
 ResetConsoleColors();
 displayPlan.RefreshDisplay(state, scrollStatus);
 ShowMessage("Use arrow keys to move, shift + arrow keys to shoot, 'q' to quit.");
+
+// Main loop
 while (true)
 {
-    cycleTimer.AwaitCycle();
-
-    // Check if buffer size changed
-    bufferStats = bufferStats.Refresh();
-    displayPlan = new(state, scrollStatus, bufferStats);
-
-    // React to any key input
-    if (Console.KeyAvailable) keyInfo = Console.ReadKey();
-    if (keyInfo.KeyChar == 'q') break;
-    state = state.RefreshCyclables(keyInfo.ToActionInput());
-    keyInfo = default;
-
-    // Check if we need to move the focus of the screen
-    scrollStatus = scrollStatus.Update(state.Player.Location, bufferStats);
-        
-    // Display current state
-    displayPlan.RefreshDisplay(newState: state, newScrollStatus: scrollStatus);
-
-    // Move the cursor so it doesn't always appear next to the player
-    Console.SetCursorPosition(1, Console.BufferHeight - 1);
+    cycleTimer.AwaitCycle(); // Update only once per 20 ms
+    bufferStats = bufferStats.Refresh(); // Check if buffer size changed
+    displayPlan = new(state, scrollStatus, bufferStats); // save current screen layout
+    keyInfo = GetKeyIfAny(); // Get next key input
+    if (keyInfo.KeyChar == 'q') break; // Quit on q
+    state = state.RefreshCyclables(keyInfo.ToActionInput()); // Update level state using key input
+    scrollStatus = scrollStatus.Update(state.Player.Location, bufferStats); // Plan to scroll screen if needed
+    displayPlan.RefreshDisplay(newState: state, newScrollStatus: scrollStatus); // Re-display anything that changed
+    Console.SetCursorPosition(1, Console.BufferHeight - 1); // Move the cursor so it doesn't always appear next to the player
 }
+ResetConsoleColors(); // Cleanup
 
-ResetConsoleColors();
+// Helper Functions
 static void ShowMessage(string message)
 {
     ResetConsoleColors();
@@ -47,6 +41,12 @@ static void ShowMessage(string message)
     Console.WriteLine(message);
 }
 
+static ConsoleKeyInfo GetKeyIfAny()
+{
+    ConsoleKeyInfo nextKey = default;
+    if (Console.KeyAvailable) nextKey = Console.ReadKey();
+    return nextKey;
+}
 static void ResetConsoleColors()
 {
     Console.ForegroundColor = ConsoleColor.White;
