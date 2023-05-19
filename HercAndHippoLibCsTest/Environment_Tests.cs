d2d7d1e5f5801a1a699aa10a38226df2b50a9e25
Matthew Dirks <majadirks@gmail.com>
2023-05-18 21:18:30 -0700

    Wrote unit test: wall stops player

---
+++ HercAndHippoLibCsTest/Environment_Tests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HercAndHippoLibCsTest
+{
+    [TestClass]
+    public class Environment_Tests
+    {
+
+        [TestMethod]
+        public void WallStopsPlayer_Test()
+        {
+            // Arrnage
+            Player initial = Player.Default((2, 2));
+            Player secondPosition = Player.Default((3, 2));
+            Player impossiblePosition = Player.Default((4, 2));
+            Wall wall = new(ConsoleColor.White, (4, 2));
+            Wall boundary = new(ConsoleColor.White, (5, 2));
+            Level level = new(initial, new HashSet<IDisplayable> { wall, boundary });
+
+            // Act and Assert
+            level = level.RefreshCyclables(ActionInput.MoveEast);
+            Assert.IsTrue(level.Contains(secondPosition)); // Player is adjacent to to wall after moving east
+            level = level.RefreshCyclables(ActionInput.MoveEast);
+            Assert.IsTrue(level.Contains(secondPosition)); // Wall has blocked further eastward movement.
+            Assert.IsFalse(level.Contains(impossiblePosition));
+        }
+
+    }
+}
