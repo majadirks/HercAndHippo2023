41f4fa10306e3c8c6c5b17beee30d9d72c4de267
Matthew Dirks <majadirks@gmail.com>
2023-05-18 21:29:59 -0700

    Wrote test: wall stops bullet

--- HercAndHippoLibCsTest/Environment_Tests.cs
+++ HercAndHippoLibCsTest/Environment_Tests.cs
@@ -13,13 +13,13 @@ namespace HercAndHippoLibCsTest
         [TestMethod]
         public void WallStopsPlayer_Test()
         {
-            // Arrnage
+            // Arrange
             Player initial = Player.Default((2, 2));
             Player secondPosition = Player.Default((3, 2));
             Player impossiblePosition = Player.Default((4, 2));
             Wall wall = new(ConsoleColor.White, (4, 2));
-            Wall boundary = new(ConsoleColor.White, (5, 2));
-            Level level = new(initial, new HashSet<IDisplayable> { wall, boundary });
+            Wall corner = new(ConsoleColor.White, (10, 10));
+            Level level = new(initial, new HashSet<IDisplayable> { wall, corner });
 
             // Act and Assert
             level = level.RefreshCyclables(ActionInput.MoveEast);
@@ -27,6 +27,28 @@ namespace HercAndHippoLibCsTest
             level = level.RefreshCyclables(ActionInput.MoveEast);
             Assert.IsTrue(level.Contains(secondPosition)); // Wall has blocked further eastward movement.
             Assert.IsFalse(level.Contains(impossiblePosition));
+        }
+
+        [TestMethod]
+        public void WallStopsBullet_Test()
+        {
+            // Arrange
+            Player player = Player.Default((2, 2)) with { AmmoCount = 1 };
+            Wall wall = new(ConsoleColor.White, (4, 2));
+            Wall corner = new(ConsoleColor.White, (10, 10));
+            Level level = new(player, new HashSet<IDisplayable> { wall, corner });
+            Bullet initialBullet = new((3, 2), Direction.East);
+            Bullet bulletOverWall = new(wall.Location, Direction.East);
+
+            // Act
+            level = level.RefreshCyclables(ActionInput.ShootEast);
+            Assert.IsTrue(level.Contains(initialBullet)); // Bullet is between player and wall
+            Assert.IsFalse(level.Contains(bulletOverWall));
+            // On next cycle, bullet moves to overlap wall. On subsequent cycle, bullet is gone.
+            level = level.RefreshCyclables(ActionInput.NoAction).RefreshCyclables(ActionInput.NoAction);
+            
+            // Assert
+            Assert.IsFalse(level.LevelObjects.Where(b => b is Bullet).Any());
         }
 
     }
