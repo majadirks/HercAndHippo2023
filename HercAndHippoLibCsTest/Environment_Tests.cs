4948a75a6e6c3369f717945363421369f44aaf8f
Matthew Dirks <majadirks@gmail.com>
2023-05-22 18:28:55 -0700

    Wrote unit tests for door behavior

--- HercAndHippoLibCsTest/Environment_Tests.cs
+++ HercAndHippoLibCsTest/Environment_Tests.cs
@@ -89,5 +89,70 @@ namespace HercAndHippoLibCsTest
             Assert.IsFalse(level.LevelObjects.Where(b => b is Bullet).Any());
         }
 
+        [TestMethod]
+        public void DoorStopsBullet_Test()
+        {
+            // Arrange
+            Player player = Player.Default((2, 2)) with { AmmoCount = 1 };
+            Door door = new(ConsoleColor.DarkMagenta, (4, 2));
+            Wall corner = new(ConsoleColor.White, (10, 10));
+            Level level = new(player, new HashSet<IDisplayable> { door, corner });
+            Bullet initialBullet = new((3, 2), Direction.East);
+            Bullet bulletOverWall = new(door.Location, Direction.East);
+
+            // Act
+            level = level.RefreshCyclables(ActionInput.ShootEast);
+            Assert.IsTrue(level.Contains(initialBullet)); // Bullet is between player and wall
+            Assert.IsFalse(level.Contains(bulletOverWall));
+            // On next cycle, bullet moves to overlap wall. On subsequent cycle, bullet is gone.
+            level = level.RefreshCyclables(ActionInput.NoAction).RefreshCyclables(ActionInput.NoAction);
+
+            // Assert
+            Assert.IsFalse(level.LevelObjects.Where(b => b is Bullet).Any());
+        }
+
+
+        [TestMethod]
+        public void DoorStopsPlayerWithoutKey_Test()
+        {
+            // Arrange
+            Player initial = Player.Default((2, 2));
+            Player secondPosition = Player.Default((3, 2));
+            Player playerAtDoor = Player.Default((4, 2));
+            Door door = new(ConsoleColor.Cyan, (4, 2));
+            Wall corner = new(ConsoleColor.White, (10, 10));
+            Level level = new(initial, new HashSet<IDisplayable> { door, corner });
+
+            // Act and Assert
+            level = level.RefreshCyclables(ActionInput.MoveEast);
+            Assert.IsTrue(level.Contains(secondPosition)); // Player is adjacent to to door after moving east
+            level = level.RefreshCyclables(ActionInput.MoveEast);
+            Assert.IsTrue(level.Contains(secondPosition)); // door has blocked further eastward movement.
+            Assert.IsFalse(level.Contains(playerAtDoor));
+        }
+
+        [TestMethod]
+        public void DoorYieldsToPlayerWithKey_Test()
+        {
+            // Arrange
+            Player initial = Player.Default((2, 2));
+           
+            Player playerAtDoor = Player.Default((4, 2));
+            Door door = new(ConsoleColor.Cyan, (4, 2));
+            Key key = new(ConsoleColor.Cyan, (3, 2));
+            Player secondPosition = Player.Default((3, 2)) with { Inventory = new Inventory(starterItem: key) };
+            Wall corner = new(ConsoleColor.White, (10, 10));
+            Level level = new(initial, new HashSet<IDisplayable> { door, key, corner });
+
+            // Act and Assert
+            level = level.RefreshCyclables(ActionInput.MoveEast);
+            Assert.IsTrue(level.Contains(secondPosition)); // Player is adjacent to to door after moving east
+            Assert.IsTrue(level.Player.Has<Key>(ConsoleColor.Cyan));// Player has picked up the key
+            level = level.RefreshCyclables(ActionInput.MoveEast);
+            Assert.IsFalse(level.Contains(secondPosition)); // door has not blocked further eastward movement.
+            Assert.IsTrue(level.Contains(playerAtDoor)); // Player has moved over door
+            Assert.IsFalse(level.Contains(door)); // No more door!
+            Assert.IsFalse(level.Player.Has<Key>(ConsoleColor.Cyan)); // Player no longer has key
+        }
     }
 }
