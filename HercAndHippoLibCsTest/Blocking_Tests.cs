dea84a13376edd4d3c796043162a7ab157c89ca2
Matthew Dirks <majadirks@gmail.com>
2023-05-29 12:28:04 -0700

    New unit test BlockedOnBoundaries_Test() passes.

--- HercAndHippoLibCsTest/Blocking_Tests.cs
+++ HercAndHippoLibCsTest/Blocking_Tests.cs
@@ -42,7 +42,32 @@
             Assert.IsTrue(player.IsBlocked(blockWest, Direction.West));
         }
 
-        // ToDo: verify blocked on level boundaries
+        [TestMethod]
+        public void BlockedOnBoundaries_Test()
+        {
+            // Arrange
+            Player player = Player.Default((5, 5));
+            Noninteractor northNi = new((5, Row.MIN_ROW));
+            Noninteractor eastNi = new((10, 5));
+            Noninteractor southNi = new((5, 10));
+            Noninteractor westNi = new((Column.MIN_COL, 5));
+            Level level = new(player, new HashSet<HercAndHippoObj> {northNi, eastNi, southNi, westNi});
 
+            // Assert
+            Assert.AreEqual(10, level.Width);
+            Assert.AreEqual(10, level.Height);
+
+            Assert.IsTrue(northNi.IsBlocked(level, Direction.North));
+            Assert.IsFalse(northNi.IsBlocked(level, Direction.South));
+
+            Assert.IsTrue(eastNi.IsBlocked(level, Direction.East));
+            Assert.IsFalse(eastNi.IsBlocked(level, Direction.West));
+
+            Assert.IsTrue(southNi.IsBlocked(level, Direction.South));
+            Assert.IsFalse(southNi.IsBlocked(level, Direction.North));
+
+            Assert.IsTrue(westNi.IsBlocked(level, Direction.West));
+            Assert.IsFalse(westNi.IsBlocked(level, Direction.East));
+        }
     }
 }
