4634aac824f28f8cc66746b984a0ea29844a2d1b
Matthew Dirks <majadirks@gmail.com>
2023-05-29 14:47:18 -0700

    Add color enum

---
+++ HercAndHippoLibCs/Color.cs
@@ -0,0 +1,9 @@
+﻿namespace HercAndHippoLibCs
+{
+    public enum Color
+    {
+        White, Black, 
+        Red, Yellow, Green, Cyan, Blue, Magenta,
+        DarkRed, DarkYellow, DarkGreen, DarkCyan, DarkBlue, DarkMagenta
+    }
+}
