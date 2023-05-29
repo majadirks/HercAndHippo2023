0d666e0349d31303c62d244623a80d47fa891452
Matthew Dirks <majadirks@gmail.com>
2023-05-29 11:56:29 -0700

    Wrote an IsBlocked method. All extant unit tests pass. Now to write some new tests.

--- HercAndHippoLibCs/HercAndHippoObj.cs
+++ HercAndHippoLibCs/HercAndHippoObj.cs
@@ -6,11 +6,49 @@ namespace HercAndHippoLibCs
     {
         public virtual bool IsBlocking => this is ILocatable;
 
-        //public bool IsBlocked(Direction where)
-        //{
-        //    if (this is not ILocatable loc) return false;
-            
-        //}
+        public bool IsBlocked(Level level, Direction where)
+            => this is ILocatable locatable && 
+            where switch
+            {
+                Direction.North => IsBlockedNorth(level, locatable),
+                Direction.East => IsBlockedEast(level, locatable),
+                Direction.South => IsBlockedSouth(level, locatable),
+                Direction.West => IsBlockedWest(level, locatable),
+                _ => false
+            };
+        
+
+        private static bool IsBlockedEast(Level level, ILocatable locatable)
+        {
+            Column nextEast = locatable.Location.Col.NextEast(level.Width);
+            Location eastLoc = (nextEast, locatable.Location.Row);
+            IEnumerable<HercAndHippoObj> blockers = level.ObjectsAt(eastLoc);
+            return blockers.Where(bl => bl.IsBlocking).Any();
+        }
+
+        private static bool IsBlockedWest(Level level, ILocatable locatable)
+        {
+            Column nextWest = locatable.Location.Col.NextWest();
+            Location westLoc = (nextWest, locatable.Location.Row);
+            IEnumerable<HercAndHippoObj> blockers = level.ObjectsAt(westLoc);
+            return blockers.Where(bl => bl.IsBlocking).Any();
+        }
+
+        private static bool IsBlockedNorth(Level level, ILocatable locatable)
+        {
+            Row nextNorth = locatable.Location.Row.NextNorth();
+            Location northLoc = (locatable.Location.Col, nextNorth);
+            IEnumerable<HercAndHippoObj> blockers = level.ObjectsAt(northLoc);
+            return blockers.Where(bl => bl.IsBlocking).Any();
+        }
+
+        private static bool IsBlockedSouth(Level level, ILocatable locatable)
+        {
+            Row nextSouth = locatable.Location.Row.NextSouth(level.Height);
+            Location southLoc = (locatable.Location.Col, nextSouth);
+            IEnumerable<HercAndHippoObj> blockers = level.ObjectsAt(southLoc);
+            return blockers.Where(bl => bl.IsBlocking).Any();
+        }
 
     }
 
