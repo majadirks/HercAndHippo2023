ab34e42bb82fc5b4cd5f375b796acee4bc74e0da
Matthew Dirks <majadirks@gmail.com>
2023-12-28 09:09:57 -0800

    Constant DEFAULT_WAIT_CYCLES

--- HercAndHippoLibCs/Gravity.cs
+++ HercAndHippoLibCs/Gravity.cs
@@ -1,6 +1,7 @@
 ﻿namespace HercAndHippoLibCs;
 public record Gravity(int Strength, int WaitCycles)
 {
-    public static implicit operator Gravity(int strength) => new(Strength: Math.Max(strength, 0), WaitCycles: 1);
+    public const int DEFAULT_WAIT_CYCLES = 2;
+    public static implicit operator Gravity(int strength) => new(Strength: Math.Max(strength, 0), WaitCycles: DEFAULT_WAIT_CYCLES);
     public static implicit operator int(Gravity gravity) => gravity.Strength;  
 }
