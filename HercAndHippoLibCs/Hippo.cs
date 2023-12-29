9bea2241b262297d3f32d6017874b363cedff495
Matthew Dirks <majadirks@gmail.com>
2023-12-28 20:40:05 -0800

    remove gravity check from Hippo cycle

--- HercAndHippoLibCs/Hippo.cs
+++ HercAndHippoLibCs/Hippo.cs
@@ -18,8 +18,6 @@ public record Hippo(Location Location, Health Health, bool LockedToPlayer) : Her
             return Behaviors.Die(level, this);
         else if (LockedToPlayer && actionInput == ActionInput.DropHippo)
             return PutDown(level);
-        else if (level.HasGravity && LockedToPlayer && actionInput == ActionInput.MoveSouth)
-            return PutDown(level);
         else if (LockedToPlayer)
             return LockAbovePlayer(level);
         else
