6abba84a4080d43305d3809bcf46e69555675f3d
Matthew Dirks <majadirks@gmail.com>
2023-12-31 15:06:08 -0800

    Move HashSetExtensions to their own file

---
+++ HercAndHippoLibCs/HashSetExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace HercAndHippoLibCs;
+
+public static class HashSetExtensions
+{
+    public static HashSet<T> AddObject<T>(this HashSet<T> collection, T toAdd) => new(collection, collection.Comparer) { toAdd };
+    public static HashSet<T> AddObjects<T>(this HashSet<T> collection, params T?[] toAdd) 
+        => new(collection.Concat(toAdd.Where(item => item !=null).Cast<T>()), collection.Comparer);
+    public static HashSet<T> RemoveObject<T>(this HashSet<T> collection, T toRemove)
+    {
+        HashSet<T> removed = new(collection, collection.Comparer);
+        removed.Remove(toRemove);
+        return removed;
+    }
+}
+
+
--- HercAndHippoLibCs/Level.cs
+++ HercAndHippoLibCs/Level.cs
@@ -99,18 +99,3 @@ public class Level
     private static int GetWidth(HashSet<HercAndHippoObj> ds) => ds.Where(ds => ds is ILocatable d).Cast<ILocatable>().Select(d => d.Location.Col).Max() ?? 0;
     private static int GetHeight(HashSet<HercAndHippoObj> ds) => ds.Where(ds => ds is ILocatable d).Cast<ILocatable>().Select(d => d.Location.Row).Max() ?? 0;
 }
-
-public static class HashSetExtensions
-{
-    public static HashSet<T> AddObject<T>(this HashSet<T> collection, T toAdd) => new(collection, collection.Comparer) { toAdd };
-    public static HashSet<T> AddObjects<T>(this HashSet<T> collection, params T?[] toAdd) 
-        => new(collection.Concat(toAdd.Where(item => item !=null).Cast<T>()), collection.Comparer);
-    public static HashSet<T> RemoveObject<T>(this HashSet<T> collection, T toRemove)
-    {
-        HashSet<T> removed = new(collection, collection.Comparer);
-        removed.Remove(toRemove);
-        return removed;
-    }
-}
-
-
