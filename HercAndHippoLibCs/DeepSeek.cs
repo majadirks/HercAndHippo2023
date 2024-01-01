766376aef8200fba2ef0f9e8ebf8946fd1b467d1
Matthew Dirks <majadirks@gmail.com>
2024-01-01 14:23:59 -0800

    Create separate class for DeepSeek logic (#17)
    * Move DeepSeek behavior to its own file
    
    * DeepSeek returns new record type DeepSeekResults

---
+++ HercAndHippoLibCs/DeepSeek.cs
@@ -0,0 +1,115 @@
+﻿using static HercAndHippoLibCs.Location;
+
+namespace HercAndHippoLibCs;
+
+public record DeepSeekResults(Direction Direction, Location Location, int Metric); 
+
+public static class DeepSeekExtensions
+{
+    private record DeepSeekParams(HercAndHippoObj Hho, Level Level, int Depth, Location CameFrom);
+    private static readonly Dictionary<DeepSeekParams, DeepSeekResults> deepSeekCache = new();
+
+    /// <summary>>
+    /// Similar to seek, but recursively attempts to minimize 
+    /// distance to player after (lookahead) moves ahead
+    /// </summary>
+    public static DeepSeekResults DeepSeek<T>(this T hho, Level level, int depth, Location cameFrom) where T : HercAndHippoObj, ILocatable
+        => DeepSeek<T>(new(hho, level, depth, cameFrom));
+
+    private static DeepSeekResults DeepSeek<T>(DeepSeekParams ssps) where T : HercAndHippoObj, ILocatable
+    {
+        if (deepSeekCache.TryGetValue(ssps, out DeepSeekResults? value) && value != null)
+        {
+            return value;
+        }
+
+        T hho = (T)ssps.Hho;
+        Level level = ssps.Level;
+        int depth = ssps.Depth;
+        Player player = level.Player;
+        Location cameFrom = ssps.CameFrom;
+        int initialDistance = ManhattanDistance(hho.Location, player.Location);
+        if (hho.Location == player.Location)
+        {
+            return new DeepSeekResults(Direction.Idle, hho.Location, 0);
+        }
+        else if (depth == 0)
+        {
+            return new DeepSeekResults(Direction.Idle, hho.Location, initialDistance);
+        }
+        Location nextNorth = new(hho.Location.Col, hho.Location.Row.NextNorth());
+        Location nextEast = new(hho.Location.Col.NextEast(level.Width), hho.Location.Row);
+        Location nextSouth = new(hho.Location.Col, hho.Location.Row.NextSouth(level.Height));
+        Location nextWest = new(hho.Location.Col.NextWest(), hho.Location.Row);
+
+        int northDist = int.MaxValue;
+        int eastDist = int.MaxValue;
+        int southDist = int.MaxValue;
+        int westDist = int.MaxValue;
+
+        if (!hho.MotionBlockedTo(level, Direction.North) && cameFrom != nextNorth)
+        {
+            T newHho = hho with { Location = nextNorth };
+            Level newLevel = level.Replace(hho, newHho);
+            var northResults = DeepSeek(newHho, newLevel, depth - 1, cameFrom: hho.Location);
+            northDist = northResults.Metric;
+        }
+        if (!hho.MotionBlockedTo(level, Direction.East) && cameFrom != nextEast)
+        {
+            T newHho = hho with { Location = nextEast };
+            Level newLevel = level.Replace(hho, newHho);
+            var eastResults = DeepSeek(newHho, newLevel, depth - 1, cameFrom: hho.Location);
+            eastDist = eastResults.Metric;
+        }
+        if (!hho.MotionBlockedTo(level, Direction.South) && cameFrom != nextSouth)
+        {
+            T newHho = hho with { Location = nextSouth };
+            Level newLevel = level.Replace(hho, newHho);
+            var southResults = DeepSeek(newHho, newLevel, depth - 1, cameFrom: hho.Location);
+            southDist = southResults.Metric;
+        }
+        if (!hho.MotionBlockedTo(level, Direction.West) && cameFrom != nextWest)
+        {
+            T newHho = hho with { Location = nextWest };
+            Level newLevel = level.Replace(hho, newHho);
+            var westResults = DeepSeek(newHho, newLevel, depth - 1, cameFrom: hho.Location);
+            westDist = westResults.Metric;
+        }
+
+        int[] distances = new int[] { northDist, eastDist, southDist, westDist };
+        int newDist = distances.Min();
+        Location newLoc = hho.Location;
+        Direction newDir = Direction.Idle;
+        if (newDist == int.MaxValue || newDist == initialDistance)
+        {
+            newLoc = hho.Location;
+            newDir = Direction.Idle;
+        }
+        else if (newDist == northDist)
+        {
+            newLoc = nextNorth;
+            newDir = Direction.North;
+        }
+        else if (newDist == eastDist)
+        {
+            newLoc = nextEast;
+            newDir = Direction.East;
+        }
+        else if (newDist == southDist)
+        {
+            newLoc = nextSouth;
+            newDir = Direction.South;
+        }
+        else if (newDist == westDist)
+        {
+            newLoc = nextWest;
+            newDir = Direction.West;
+        }
+        else
+            throw new NotSupportedException($"An unexpected error occurred in method {nameof(DeepSeek)}.");
+        DeepSeekResults results = new(Direction: newDir, Location: newLoc, Metric: newDist);
+
+        deepSeekCache.Add(ssps, results);
+        return results;
+    }
+}
---
+++ HercAndHippoLibCsTest/DeepSeek_Tests.cs
@@ -0,0 +1,81 @@
+﻿namespace HercAndHippoLibCsTest;
+
+[TestClass]
+public class DeepSeek_Tests
+{
+    [TestMethod]
+    public void DeepSeek_Depth3_Test()
+    {
+        // Arrange
+        int depth = 3;
+        Level level = new(
+            player: Player.Default(5, 1),
+            hippo: null,
+            gravity: Gravity.Default,
+            secondaryObjects: new()
+            {
+                new Wall(Color.Yellow, (5,2)),
+                new Wall(Color.Black, (20,20)),
+                new Wall(Color.Yellow, (9,1)),
+                new Bullet((10, 1), Direction.Idle)
+            });
+        Bullet initial = FindBullet();
+        Assert.AreEqual(new Location(10, 1), initial.Location);
+        Assert.AreEqual(Direction.Idle, initial.Whither);
+
+        // Act
+        DeepSeekResults results = initial.DeepSeek(level, depth, cameFrom: initial.Location);
+        level = level.Replace(initial, initial with { Location = results.Location });
+
+        // Assert: West and North are blocked, so went South
+        Bullet iterated = FindBullet();
+        Assert.AreEqual(new Location(10, 2), iterated.Location);
+        Assert.AreEqual(4, results.Metric);
+
+        // Act
+        results = iterated.DeepSeek(level, depth, cameFrom: iterated.Location);
+        level = level.Replace(iterated,
+            iterated with { Location = results.Location });
+        iterated = FindBullet();
+
+        // Assert: North is blocked, so went West
+        Assert.AreEqual(new Location(9, 2), iterated.Location);
+        Assert.AreEqual(3, results.Metric);
+
+        // Act
+        results = iterated.DeepSeek(level, depth, cameFrom: iterated.Location);
+        level = level.Replace(iterated,
+            iterated with { Location = results.Location });
+        iterated = FindBullet();
+
+        // Assert: North is blocked, so went West
+        Assert.AreEqual(new Location(8, 2), iterated.Location);
+        Assert.AreEqual(2, results.Metric);
+
+        // Act
+        results = iterated.DeepSeek(level, depth, cameFrom: iterated.Location);
+        level = level.Replace(iterated,
+            iterated with { Location = results.Location });
+        iterated = FindBullet();
+
+        results = iterated.DeepSeek(level, depth, cameFrom: iterated.Location);
+        level = level.Replace(iterated,
+            iterated with { Location = results.Location });
+        iterated = FindBullet();
+
+        results = iterated.DeepSeek(level, depth, cameFrom: iterated.Location);
+        level = level.Replace(iterated,
+            iterated with { Location = results.Location });
+        iterated = FindBullet();
+
+        results = iterated.DeepSeek(level, depth, cameFrom: iterated.Location);
+        level = level.Replace(iterated,
+            iterated with { Location = results.Location });
+        iterated = FindBullet();
+
+        Assert.AreEqual(level.Player.Location, iterated.Location);
+
+        // Local method
+        Bullet FindBullet() => (Bullet)level.LevelObjects.Where(obj => obj is Bullet).Single();
+    }
+}
