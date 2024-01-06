bcb7dd5765c2087cff00f8ad65a2161bbdca747b
Matthew Dirks <majadirks@gmail.com>
2024-01-06 14:26:22 -0800

    Separate display logic classes to their own files

---
+++ HercAndHippoConsole/BufferStats.cs
@@ -0,0 +1,31 @@
+﻿namespace HercAndHippoConsole;
+
+internal class BufferStats
+{
+    public bool BufferSizeChanged { get; private set; }
+    public int BufferWidth { get; private set; }
+    public int BufferHeight { get; private set; }
+    public BufferStats(bool bufferSizeChanged, int bufferWidth, int bufferHeight)
+    {
+        BufferSizeChanged = bufferSizeChanged;
+        BufferWidth = bufferWidth;
+        BufferHeight = bufferHeight;
+    }
+    public void Refresh()
+    {
+        int newBH = Console.BufferHeight;
+        int newBW = Console.BufferWidth;
+
+        BufferSizeChanged = BufferHeight != newBH || BufferWidth != newBW;
+        BufferWidth = newBW;
+        BufferHeight = newBH;
+   
+    }
+
+    public void ForceRefresh()
+    {
+        Refresh();
+        BufferSizeChanged = true;
+    }
+    
+}
--- HercAndHippoConsole/DisplayPlan.cs
+++ HercAndHippoConsole/DisplayPlan.cs
@@ -3,45 +3,6 @@ using static HercAndHippoConsole.Constants;
 
 namespace HercAndHippoConsole;
 
-internal static class DisplayUtilities
-{
-    public static void ResetConsoleColors()
-    {
-        Console.ForegroundColor = ConsoleColor.White;
-        Console.BackgroundColor = ConsoleColor.Black;
-    }
-    
-}
-internal class BufferStats
-{
-    public bool BufferSizeChanged { get; private set; }
-    public int BufferWidth { get; private set; }
-    public int BufferHeight { get; private set; }
-    public BufferStats(bool bufferSizeChanged, int bufferWidth, int bufferHeight)
-    {
-        BufferSizeChanged = bufferSizeChanged;
-        BufferWidth = bufferWidth;
-        BufferHeight = bufferHeight;
-    }
-    public void Refresh()
-    {
-        int newBH = Console.BufferHeight;
-        int newBW = Console.BufferWidth;
-
-        BufferSizeChanged = BufferHeight != newBH || BufferWidth != newBW;
-        BufferWidth = newBW;
-        BufferHeight = newBH;
-   
-    }
-
-    public void ForceRefresh()
-    {
-        Refresh();
-        BufferSizeChanged = true;
-    }
-    
-}
-
 internal class DisplayPlan
 {
     private readonly IConsoleDisplayable[,] planArray;
