5195e7ceb8c0d4abb2e9e845b7f9aad0cfe7d591
Matthew Dirks <majadirks@gmail.com>
2024-01-19 15:23:44 -0800

    slow things down slightly

--- HercAndHippoConsole/Program.cs
+++ HercAndHippoConsole/Program.cs
@@ -6,7 +6,7 @@ public class Program
     static void Main()
     {
         GameController keyboard = new KeyboardController();
-        DisplayLoop mainLoop = new(state: DemoLevels.IntroducingTheHippo, frequency_hz: 40);
+        DisplayLoop mainLoop = new(state: DemoLevels.IntroducingTheHippo, frequency_hz: 35);
         mainLoop.RunGame(controller: keyboard);
     }
 }
