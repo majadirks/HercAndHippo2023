e00f6762a50b3204fdca3aefcdcb074269a896b2
Matthew Dirks <majadirks@gmail.com>
2024-01-19 21:32:30 -0800

    cosmetics

--- HercAndHippoConsole/StatusBar.cs
+++ HercAndHippoConsole/StatusBar.cs
@@ -1,51 +1,38 @@
 ﻿using HercAndHippoLibCs;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Windows.Media.PlayTo;
+namespace HercAndHippoConsole;
 
-namespace HercAndHippoConsole
+public class StatusBar
 {
-    public class StatusBar
+    private readonly int margin;
+    public StatusBar(int margin)
     {
-        private readonly int margin;
-        public StatusBar(int margin)
-        {
-            this.margin = margin;
-        }
-        /*
-         * - Status bar:
-   Player: Health, ammo, inventory, Kinetic Energy, player location,
-   Hippo: hippo location if any, hippo locked, 
-   Console: scroll status, buffer stats, estimated cycle time, use parallel, refresh types (percents)
-         */
-        public void ShowStatus(Level state)
-        {          
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.BackgroundColor = ConsoleColor.Blue;
+        this.margin = margin;
+    }
 
-            Console.SetCursorPosition(0, Console.BufferHeight - margin);
-            Player player = state.Player;
-            Hippo? hippo = state.Hippo;
-            OverwriteLine($"{player.Health}, {player.AmmoCount}, {player.Velocity}, Energy: {player.KineticEnergy}");
-            OverwriteLine(player.Inventory.ToString());
-            string hippoStr = hippo == null ?
-                "No hippo present." :
-                hippo.LockedToPlayer ? $"Hippo locked, health = {hippo.Health}." : $"Hippo loose, health = {hippo.Health}.";
-            string levelStr = $"Level: gravity {state.Gravity}, cycle count {state.Cycles}.";
-            OverwriteLine(hippoStr + " " + levelStr);
-            OverwriteLine($"Display plan stats: {FutureStates.GetCacheStats()}");
-            if (state.GetMessage() is Message message)
-                OverwriteLine(message.Text);
-            else
-                OverwriteLine(" ");
-        }
+    public void ShowStatus(Level state)
+    {          
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.BackgroundColor = ConsoleColor.Blue;
 
-        private void OverwriteLine(string str)
-        {
-            Console.WriteLine(str + new string(' ', Console.WindowWidth - str.Length));
-        }
+        Console.SetCursorPosition(0, Console.BufferHeight - margin);
+        Player player = state.Player;
+        Hippo? hippo = state.Hippo;
+        OverwriteLine($"{player.Health}, {player.AmmoCount}, {player.Velocity}, Energy: {player.KineticEnergy}");
+        OverwriteLine(player.Inventory.ToString());
+        string hippoStr = hippo == null ?
+            "No hippo present." :
+            hippo.LockedToPlayer ? $"Hippo locked, health = {hippo.Health}." : $"Hippo loose, health = {hippo.Health}.";
+        string levelStr = $"Level: gravity {state.Gravity}, cycle count {state.Cycles}.";
+        OverwriteLine(hippoStr + " " + levelStr);
+        OverwriteLine($"Display plan stats: {FutureStates.GetCacheStats()}");
+        if (state.GetMessage() is Message message)
+            OverwriteLine(message.Text);
+        else
+            OverwriteLine(" ");
+    }
+
+    private static void OverwriteLine(string str)
+    {
+        Console.WriteLine(str + new string(' ', Console.WindowWidth - str.Length));
     }
 }
