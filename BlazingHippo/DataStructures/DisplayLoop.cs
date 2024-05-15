932384f1a3436f1f67257c36bb9f398f253fc1a0
Matthew Dirks <majadirks@gmail.com>
2024-05-14 19:57:23 -0700

    misc

--- BlazingHippo/DataStructures/DisplayLoop.cs
+++ BlazingHippo/DataStructures/DisplayLoop.cs
@@ -8,18 +8,19 @@ internal class DisplayLoop
     private readonly IProgress<bool> progress;
     private readonly Cycler cycleTimer;
     private readonly StatusBar statusBar;
+    private readonly GameController controller;
     public Level State { get; private set; }
     private ScrollStatus scrollStatus;
     private DisplayPlan displayPlan;
     private ActionInputPair lastActions;
     private IEnumerable<DisplayDiff> diffs;
-    private 
-    public DisplayLoop(Level state, int frequency_hz)
+    public DisplayLoop(GameController controller, Level state, int frequency_hz)
     {
         if (frequency_hz < 1)
             throw new ArgumentException($"Frequency must be >=1, but was given {frequency_hz}");
         // Initialize data
-        State = state;
+        this.State = state;
+        this.controller = controller;
         cts = new();
         progress = new Progress<bool>(handler: _ => Update());
         cycleTimer = new(frequencyHz: frequency_hz, progress, cts.Token);
@@ -29,22 +30,14 @@ internal class DisplayLoop
         lastActions = new(ActionInput.NoAction);
         statusBar = new(margin: 6);
         ThreadPool.SetMinThreads(workerThreads: 15, completionPortThreads: 0);
-
-        // Initialize display
-        ResetConsoleColors();
         diffs = displayPlan.GetDiffs(displayPlan);
         displayPlan.RefreshDisplay(diffs);
     }
-    private void Update()
-    {
-        Console.WriteLine(State.ToString());
-    }
 
-    public void RunGame(GameController controller)
+    private void Update()
     {
         DisplayPlan nextDisplayPlan;
         FutureStates futures;
-        bool refreshed;
 
         // Main loop
         while (State.WinState == WinState.InProgress)
@@ -53,23 +46,13 @@ internal class DisplayLoop
                 initialPlan: displayPlan,
                 initialState: State,
                 scrollStatus: scrollStatus,
-                bufferStats: bufferStats,
                 mostRecentInputs: lastActions); // plan for possible next states
-            cycleTimer.AwaitCycle(); // Update once per 20 ms, return key input
-            bufferStats.Refresh(); // Check if buffer size changed
-            displayPlan = new(State, scrollStatus, bufferStats); // save current screen layout
+            displayPlan = new(State, scrollStatus); // save current screen layout
             lastActions = controller.NextAction(State);
-            if (lastActions.Where(a => a == ActionInput.Quit).Any()) break;
+            if (lastActions.Where(a => a == ActionInput.Quit).Any())
+                break;
             (State, scrollStatus, diffs) = futures.GetFutureDiffs(lastActions);
-            refreshed = displayPlan.RefreshDisplay(diffs); // Re-display anything that changed
-
-            while (!refreshed)
-            {
-                bufferStats.ForceRefresh();
-                nextDisplayPlan = new DisplayPlan(State, scrollStatus, bufferStats);
-                diffs = displayPlan.GetDiffs(nextDisplayPlan);
-                refreshed = displayPlan.RefreshDisplay(diffs);
-            }
+            displayPlan.RefreshDisplay(diffs); // Re-display anything that changed
             statusBar.ShowStatus(State);
         }
 
@@ -78,7 +61,5 @@ internal class DisplayLoop
         else
             statusBar.ShowStatus(State, "You lost! Try again!");
         Console.ReadLine();
-
-        ResetConsoleColors(); // Clean up
     }
 }
