1ae5ba115f3ef1eef03b2b0a3acdaf1e0fda16ea
Matthew Dirks <majadirks@gmail.com>
2024-05-16 21:06:16 -0700

    Control Herc with wasd in Blazor app (#40)
    Control Herc with wasd in Blazor app

--- BlazingHippo/DataStructures/Controllers.cs
+++ BlazingHippo/DataStructures/Controllers.cs
@@ -9,3 +9,38 @@ public class DoNothingController : GameController
     }
 }
 
+public class WasdController : GameController
+{
+    public const int NO_ACTION_KEY = -1;
+    private readonly Func<int> getKey;
+    public ActionInputPair PreviousAction { get; private set; }
+
+    public WasdController(Func<int> getKey)
+    {
+        this.getKey = getKey;
+        PreviousAction = ActionInput.NoAction;
+    }
+
+    public static ActionInputPair ActionFromKey(int key)
+    {
+        ActionInputPair actionInput = key switch
+        {
+            65 => ActionInput.MoveWest, // pressed "a"
+            68 => ActionInput.MoveEast, // "pressed d"
+            87 => ActionInput.MoveNorth, // "pressed w"
+            83 => ActionInput.MoveSouth, // pressed "s"
+            81 => ActionInput.Quit, // pressed "q"
+            NO_ACTION_KEY => ActionInput.NoAction,
+            _ => ActionInput.NoAction
+        };
+        return actionInput;
+    }
+
+    public override ActionInputPair NextAction(Level state)
+    {
+        int key = getKey();
+        PreviousAction = ActionFromKey(key);
+        return PreviousAction;
+    }
+}
+
--- BlazingHippo/DataStructures/DisplayLoop.cs
+++ BlazingHippo/DataStructures/DisplayLoop.cs
@@ -2,18 +2,17 @@
 using HercAndHippoLibCs;
 namespace BlazingHippo;
 
-internal class DisplayLoop : IDisposable
+internal class DisplayLoop
 {
     public const int MESSAGE_MARGIN = 3;
     private readonly CancellationTokenSource cts;
     private readonly PlayGame display;
-    private readonly StatusBar statusBar;
     private readonly GameController controller;
     public Level State { get; private set; }
     private ScrollStatus scrollStatus;
     private DisplayPlan displayPlan;
     private ActionInputPair lastActions;
-    private readonly Timer cycleTimer;
+    private readonly Timer cycleTimer; // Removing this field seems to cause cross-origin request errors ?!
     public DisplayLoop(GameController controller, Level state, int frequency_hz, PlayGame display)
     {
         if (frequency_hz < 1)
@@ -23,35 +22,29 @@ internal class DisplayLoop : IDisposable
         this.controller = controller;
         this.display = display;
         cts = new();
-
-        async void callback(object? _) => await Update();
-        cycleTimer = new Timer(callback: callback, state: null, dueTime: 1000 / frequency_hz, period: 1000 / frequency_hz);
+        async void timerCallback(object? _) => await Update();
+        cycleTimer = new Timer(callback: timerCallback, state: null, dueTime: 1000 / frequency_hz, period: 1000 / frequency_hz);
         scrollStatus = ScrollStatus.Default(state.Player.Location); 
         displayPlan = new(state, scrollStatus);
         lastActions = new(ActionInput.NoAction);
-        statusBar = new(margin: 6);
     }
 
     private async Task Update()
     {
         lastActions = controller.NextAction(State);
-        if (lastActions.Where(a => a == ActionInput.Quit).Any())
+        if (lastActions == ActionInput.Quit)
+        {
+            cts.Cancel();
             return;
+        }
         State = State.RefreshCyclables(lastActions, cts.Token);
         scrollStatus = scrollStatus.Update(State.Player.Location);
         displayPlan = new(State, scrollStatus);
         await display.Update(displayPlan);
-        statusBar.ShowStatus(State);
-
-        if (State.WinState == WinState.Won)
-            statusBar.ShowStatus(State, "Huzzah! You have won!");
-        else
-            statusBar.ShowStatus(State, "You lost! Try again!");
-
         cts.Token.ThrowIfCancellationRequested();
     }
 
-    public void Dispose()
+    public void Stop()
     {
         cts.Cancel();
     }
