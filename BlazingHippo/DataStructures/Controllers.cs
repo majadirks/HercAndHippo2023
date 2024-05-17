eba66fe386fd05bd1243d7e6403abba15826618a
Matthew Dirks <majadirks@gmail.com>
2024-05-16 17:53:07 -0700

    move key logic intoWasdController

--- BlazingHippo/DataStructures/Controllers.cs
+++ BlazingHippo/DataStructures/Controllers.cs
@@ -9,17 +9,29 @@ public class DoNothingController : GameController
     }
 }
 
-public class SimpleController : GameController
+public class WasdController : GameController
 {
-    private readonly Func<ActionInput> getAction;
-    public SimpleController(Func<ActionInput> getAction)
+    public const int NO_ACTION_KEY = -1;
+    private readonly Func<int> getKey;
+
+    public WasdController(Func<int> getKey)
     {
-        this.getAction = getAction;
+        this.getKey = getKey;
     }
 
     public override ActionInputPair NextAction(Level state)
     {
-        return getAction();
+        int key = getKey();
+        ActionInput actionInput = key switch
+        {
+            65 => ActionInput.MoveWest, // pressed "a"
+            68 => ActionInput.MoveEast, // "pressed d"
+            87 => ActionInput.MoveNorth, // "pressed w"
+            83 => ActionInput.MoveSouth, // pressed "s"
+            81 => ActionInput.Quit, // pressed "q"
+            _ => ActionInput.NoAction
+        };
+        return actionInput;
     }
 }
 
