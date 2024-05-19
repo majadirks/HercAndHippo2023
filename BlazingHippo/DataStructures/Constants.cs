b7ab8347e385b79d1ffe36b03e251f1bfdc2302e
Matthew Dirks <majadirks@gmail.com>
2024-05-18 20:13:28 -0700

    make display smaller

--- BlazingHippo/DataStructures/Constants.cs
+++ BlazingHippo/DataStructures/Constants.cs
@@ -4,6 +4,6 @@ internal class Constants
     public const int VIEW_MARGIN = 4;
     public const int MIN_DISPLAY_COL = 2;
     public const int MIN_DISPLAY_ROW = 2;
-    public const int GAME_WIDTH = 120;
-    public const int GAME_HEIGHT = 40;
+    public const int GAME_WIDTH = 80;
+    public const int GAME_HEIGHT = 30;
 }
--- BlazingHippo/DataStructures/DisplayPlan.cs
+++ BlazingHippo/DataStructures/DisplayPlan.cs
@@ -7,16 +7,12 @@ public record PlannedHho(IConsoleDisplayable Hho, int Col, int Row);
 
 public class DisplayPlan
 {
-    //private readonly IConsoleDisplayable[,] planArray;
     public PlannedHho[] Planned {get; init;}
-    //private const int MAX_COL = GAME_WIDTH - VIEW_MARGIN;
-    //private const int MAX_ROW = GAME_HEIGHT - VIEW_MARGIN;
     public static readonly Location ScreenCenter = ((GAME_WIDTH - VIEW_MARGIN) / 2, (GAME_HEIGHT - VIEW_MARGIN) / 2);
     public Level State { get; init; }
     public DisplayPlan(Level state, ScrollStatus scrollStatus)
     {
         State = state;
-        //IConsoleDisplayable[,] planArray = new IConsoleDisplayable[GAME_WIDTH, GAME_HEIGHT];
         Planned = new PlannedHho[GAME_WIDTH * GAME_HEIGHT];
         Location logicalCenter = scrollStatus.LogicalCenter;
         
@@ -27,58 +23,8 @@ public class DisplayPlan
             if (col >= MIN_DISPLAY_COL && col < GAME_WIDTH - VIEW_MARGIN && 
                 row >= MIN_DISPLAY_ROW && row < GAME_HEIGHT - VIEW_MARGIN)
             {
-                //planArray[col, row] = levelObj;
                 Planned[col + col * row] = new(Hho: levelObj, Col: col, Row: row);
             }
         }
-
-        //this.planArray = planArray;
     }
-
-
-    //public IEnumerable<DisplayDiff> GetDiffs(DisplayPlan newDisplayPlan)
-    //{
-    //    bool calculated = false;
-
-    //    while (!calculated)
-    //    {
-    //        try
-    //        {
-    //            List<DisplayDiff> diffs = [];
-    //            IConsoleDisplayable[,] oldDisplay = this.planArray;
-    //            IConsoleDisplayable[,] newDisplay = newDisplayPlan.planArray;
-                
-    //            for (int row = 0; row < MAX_ROW; row++)
-    //            {
-    //                for (int col = 0; col < MAX_COL; col++)
-    //                {
-    //                    IConsoleDisplayable? oldDisp = oldDisplay[col, row];
-    //                    IConsoleDisplayable newDisp = newDisplay[col, row];
-    //                    if (newDisp != default && oldDisp != newDisp)
-    //                    {
-    //                        var diff = new DisplayDiff(Col: col, Row: row, OldDisplayable: oldDisp, NewDisplayable: newDisp);
-    //                        // Something is here that wasn't here before, so plan to show it
-    //                        if (diff.AppearanceChanged)
-    //                            diffs.Add(diff);
-    //                    }
-    //                    if (newDisp == default && oldDisp != default)
-    //                    {
-    //                        // Something used to be here, but now nothing is here,
-    //                        // so plan to clear the spot
-    //                        diffs.Add(new DisplayDiff(Col: col, Row: row, OldDisplayable: oldDisp, NewDisplayable: null));
-    //                    }
-    //                } // end for (columns)
-    //            } // end for (rows)
-    //            calculated = true;
-    //            return diffs;
-    //        }
-    //        catch
-    //        {
-    //            calculated = false;
-    //        } 
-    //    }
-    //    if (this.diffs == null)
-    //        throw new Exception("Unexpected exception.");
-    //    return diffs;
-    //}
 }
