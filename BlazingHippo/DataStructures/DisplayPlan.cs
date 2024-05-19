d7a9a8c590ddca18b92dfbf66d6a39cf70036db3
Matthew Dirks <majadirks@gmail.com>
2024-05-18 20:41:55 -0700

    Blazor frontend scrolls (#44)
    Add scrolling to Blazor frontend

--- BlazingHippo/DataStructures/DisplayPlan.cs
+++ BlazingHippo/DataStructures/DisplayPlan.cs
@@ -3,85 +3,29 @@ using static BlazingHippo.Constants;
 
 namespace BlazingHippo;
 
-public record DisplayDiff(int Col, int Row, IConsoleDisplayable? OldDisplayable, IConsoleDisplayable? NewDisplayable)
-{
-    public bool AppearanceChanged => NewDisplayable?.Color != OldDisplayable?.Color ||
-        NewDisplayable?.BackgroundColor != OldDisplayable?.BackgroundColor ||
-        NewDisplayable?.ConsoleDisplayString != OldDisplayable?.ConsoleDisplayString;
-}
+public record PlannedHho(IConsoleDisplayable Hho, int Col, int Row);
 
 public class DisplayPlan
 {
-    private readonly IConsoleDisplayable[,] planArray;
-    private readonly IEnumerable<DisplayDiff>? diffs;
-    private const int MAX_COL = GAME_WIDTH - VIEW_MARGIN;
-    private const int MAX_ROW = GAME_HEIGHT - VIEW_MARGIN;
+    public List<PlannedHho> Planned {get; init;}
     public static readonly Location ScreenCenter = ((GAME_WIDTH - VIEW_MARGIN) / 2, (GAME_HEIGHT - VIEW_MARGIN) / 2);
     public Level State { get; init; }
     public DisplayPlan(Level state, ScrollStatus scrollStatus)
     {
         State = state;
-        IConsoleDisplayable[,] planArray = new IConsoleDisplayable[GAME_WIDTH, GAME_HEIGHT];
-        diffs = null;
+        Planned = new List<PlannedHho>(capacity: GAME_HEIGHT * GAME_WIDTH);
         Location logicalCenter = scrollStatus.LogicalCenter;
         
         foreach (IConsoleDisplayable levelObj in state.LevelObjects.Where(obj => obj is IConsoleDisplayable).Cast<IConsoleDisplayable>())
         {
-            int writeCol = ScreenCenter.Col - logicalCenter.Col + levelObj.Location.Col; 
-            int writeRow = ScreenCenter.Row - logicalCenter.Row + levelObj.Location.Row;
-            if (writeCol >= MIN_DISPLAY_COL && writeCol < GAME_WIDTH - VIEW_MARGIN && 
-                writeRow >= MIN_DISPLAY_ROW && writeRow < GAME_HEIGHT - VIEW_MARGIN)
+            int col = ScreenCenter.Col - logicalCenter.Col + levelObj.Location.Col; 
+            int row = ScreenCenter.Row - logicalCenter.Row + levelObj.Location.Row;
+            if (col >= MIN_DISPLAY_COL && col < GAME_WIDTH - VIEW_MARGIN && 
+                row >= MIN_DISPLAY_ROW && row < GAME_HEIGHT - VIEW_MARGIN)
             {
-                planArray[writeCol, writeRow] = levelObj;
+                var phho = new PlannedHho(Hho: levelObj, Col: col, Row: row);
+                Planned.Add(phho);
             }
         }
-
-        this.planArray = planArray;
-    }
-
-    public IEnumerable<DisplayDiff> GetDiffs(DisplayPlan newDisplayPlan)
-    {
-        bool calculated = false;
-
-        while (!calculated)
-        {
-            try
-            {
-                List<DisplayDiff> diffs = [];
-                IConsoleDisplayable[,] oldDisplay = this.planArray;
-                IConsoleDisplayable[,] newDisplay = newDisplayPlan.planArray;
-                
-                for (int row = 0; row < MAX_ROW; row++)
-                {
-                    for (int col = 0; col < MAX_COL; col++)
-                    {
-                        IConsoleDisplayable? oldDisp = oldDisplay[col, row];
-                        IConsoleDisplayable newDisp = newDisplay[col, row];
-                        if (newDisp != default && oldDisp != newDisp)
-                        {
-                            var diff = new DisplayDiff(Col: col, Row: row, OldDisplayable: oldDisp, NewDisplayable: newDisp);
-                            // Something is here that wasn't here before, so plan to show it
-                            if (diff.AppearanceChanged)
-                                diffs.Add(diff);
-                        }
-                        if (newDisp == default && oldDisp != default)
-                        {
-                            // Something used to be here, but now nothing is here,
-                            // so plan to clear the spot
-                            diffs.Add(new DisplayDiff(Col: col, Row: row, OldDisplayable: oldDisp, NewDisplayable: null));
-                        }
-                    } // end for (columns)
-                } // end for (rows)
-                calculated = true;
-                return diffs;
-            }
-            catch
-            {
-                calculated = false;
-            } 
-        }
-        if (this.diffs == null)
-            throw new Exception("Unexpected exception.");
-        return diffs;
     }
 }
--- BlazingHippo/DataStructures/ImageMapper.cs
+++ BlazingHippo/DataStructures/ImageMapper.cs
@@ -1,37 +1,32 @@
 ﻿using HercAndHippoConsole;
 using HercAndHippoLibCs;
 using Microsoft.AspNetCore.Components;
-using System.Runtime.CompilerServices;
+namespace BlazingHippo;
 
-namespace BlazingHippo
+public static class ImageMapper
 {
-    public static class ImageMapper
+    public const int WIDTH = 16;
+    public const int HEIGHT = 32;
+    public static IEnumerable<IConsoleDisplayable> GetDisplayables(this Level level)
+        => level.LevelObjects.Where(obj => obj is IConsoleDisplayable).Cast<IConsoleDisplayable>();
+    public static RenderFragment GetHtml(this PlannedHho phho)
     {
-        public const int WIDTH = 16;
-        public const int HEIGHT = 32;
-        public static IEnumerable<IConsoleDisplayable> GetDisplayables(this Level level)
-            => level.LevelObjects.Where(obj => obj is IConsoleDisplayable).Cast<IConsoleDisplayable>();
-        public static RenderFragment GetHtml(this HercAndHippoObj hho)
+        if (phho == null || phho.Hho == null)
+            throw new Exception("Unexpected null");
+
+        string html = phho.Hho switch
         {
-            if (hho is not IConsoleDisplayable dhho)
-                return new RenderFragment(b => b.AddMarkupContent(0, ""));
-
-            int id = hho.Id;
-            string html = hho switch
-            {
-                Hippo => $@"<img id=""{id}"" src = "".\img\hippo.jpeg"" style=""{dhho.Location()}""/>",
-                Player => $@"<img id=""{id}"" src = "".\img\herc.jpeg"" style=""{dhho.Location()}""/>",
-                Groodle => $@"<img id=""{id}"" src = "".\img\groodle.png"" style=""{dhho.Location()}""/>",
-                _ => $@"<div id=""{id}"" style=""{dhho.Location()} {dhho.Color()}"">{dhho.ConsoleDisplayString}</div>",
-            };
-            return new RenderFragment(b => b.AddMarkupContent(0, html));
-        }
-
-        private static string Location(this IConsoleDisplayable hho)
-            => $"position:absolute; left:{hho.Location.Col * WIDTH}px; top:{hho.Location.Row * HEIGHT}px; width:{WIDTH}px; height:{HEIGHT}px;";
-
-        private static string Color(this IConsoleDisplayable hho) 
-            => $"color:{hho.Color.ToHtmlColor()}; background-color:{hho.BackgroundColor.ToHtmlColor()};";
-
+            Hippo => $@"<img src = "".\img\hippo.jpeg"" style=""{phho.Location()}""/>",
+            Player => $@"<img src = "".\img\herc.jpeg"" style=""{phho.Location()}""/>",
+            Groodle => $@"<img src = "".\img\groodle.png"" style=""{phho.Location()}""/>",
+            _ => $@"<div style=""{phho.Location()} {phho.Color()}"">{phho.Hho.ConsoleDisplayString}</div>",
+        };
+        return new RenderFragment(b => b.AddMarkupContent(0, html));
     }
+    private static string Location(this PlannedHho phho)
+    => $"position:absolute; left:{phho.Col * WIDTH}px; top:{phho.Row * HEIGHT}px; width:{WIDTH}px; height:{HEIGHT}px;";
+
+    private static string Color(this PlannedHho hho)
+    => $"color:{hho.Hho.Color.ToHtmlColor()}; background-color:{hho.Hho.BackgroundColor.ToHtmlColor()};";
+
 }
