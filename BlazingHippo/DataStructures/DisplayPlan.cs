9e8a4f1bde7c267265b303a520de78cb9cfd60b3
Matthew Dirks <majadirks@gmail.com>
2024-05-18 20:37:53 -0700

    scrolling works!

--- BlazingHippo/DataStructures/DisplayPlan.cs
+++ BlazingHippo/DataStructures/DisplayPlan.cs
@@ -7,13 +7,13 @@ public record PlannedHho(IConsoleDisplayable Hho, int Col, int Row);
 
 public class DisplayPlan
 {
-    public PlannedHho[] Planned {get; init;}
+    public List<PlannedHho> Planned {get; init;}
     public static readonly Location ScreenCenter = ((GAME_WIDTH - VIEW_MARGIN) / 2, (GAME_HEIGHT - VIEW_MARGIN) / 2);
     public Level State { get; init; }
     public DisplayPlan(Level state, ScrollStatus scrollStatus)
     {
         State = state;
-        Planned = new PlannedHho[GAME_WIDTH * GAME_HEIGHT];
+        Planned = new List<PlannedHho>(capacity: GAME_HEIGHT * GAME_WIDTH);
         Location logicalCenter = scrollStatus.LogicalCenter;
         
         foreach (IConsoleDisplayable levelObj in state.LevelObjects.Where(obj => obj is IConsoleDisplayable).Cast<IConsoleDisplayable>())
@@ -23,7 +23,8 @@ public class DisplayPlan
             if (col >= MIN_DISPLAY_COL && col < GAME_WIDTH - VIEW_MARGIN && 
                 row >= MIN_DISPLAY_ROW && row < GAME_HEIGHT - VIEW_MARGIN)
             {
-                Planned[col + col * row] = new(Hho: levelObj, Col: col, Row: row);
+                var phho = new PlannedHho(Hho: levelObj, Col: col, Row: row);
+                Planned.Add(phho);
             }
         }
     }
--- BlazingHippo/DataStructures/ImageMapper.cs
+++ BlazingHippo/DataStructures/ImageMapper.cs
@@ -9,26 +9,26 @@ public static class ImageMapper
     public const int HEIGHT = 32;
     public static IEnumerable<IConsoleDisplayable> GetDisplayables(this Level level)
         => level.LevelObjects.Where(obj => obj is IConsoleDisplayable).Cast<IConsoleDisplayable>();
-    public static RenderFragment GetHtml(this HercAndHippoObj hho)
-    {
-        if (hho is not IConsoleDisplayable dhho)
-            return new RenderFragment(b => b.AddMarkupContent(0, ""));
+    //public static RenderFragment GetHtml(this HercAndHippoObj hho)
+    //{
+    //    if (hho is not IConsoleDisplayable dhho)
+    //        return new RenderFragment(b => b.AddMarkupContent(0, ""));
 
-        int id = hho.Id;
-        string html = hho switch
-        {
-            Hippo => $@"<img id=""{id}"" src = "".\img\hippo.jpeg"" style=""{dhho.Location()}""/>",
-            Player => $@"<img id=""{id}"" src = "".\img\herc.jpeg"" style=""{dhho.Location()}""/>",
-            Groodle => $@"<img id=""{id}"" src = "".\img\groodle.png"" style=""{dhho.Location()}""/>",
-            _ => $@"<div id=""{id}"" style=""{dhho.Location()} {dhho.Color()}"">{dhho.ConsoleDisplayString}</div>",
-        };
-        return new RenderFragment(b => b.AddMarkupContent(0, html));
-    }
+    //    int id = hho.Id;
+    //    string html = hho switch
+    //    {
+    //        Hippo => $@"<img id=""{id}"" src = "".\img\hippo.jpeg"" style=""{dhho.Location()}""/>",
+    //        Player => $@"<img id=""{id}"" src = "".\img\herc.jpeg"" style=""{dhho.Location()}""/>",
+    //        Groodle => $@"<img id=""{id}"" src = "".\img\groodle.png"" style=""{dhho.Location()}""/>",
+    //        _ => $@"<div id=""{id}"" style=""{dhho.Location()} {dhho.Color()}"">{dhho.ConsoleDisplayString}</div>",
+    //    };
+    //    return new RenderFragment(b => b.AddMarkupContent(0, html));
+    //}
 
     public static RenderFragment GetHtml(this PlannedHho phho)
     {
         if (phho == null || phho.Hho == null)
-            return new RenderFragment(b => b.AddMarkupContent(0, ""));
+            throw new Exception("Unexpected null");
 
         string html = phho.Hho switch
         {
@@ -36,19 +36,20 @@ public static class ImageMapper
             Player => $@"<img src = "".\img\herc.jpeg"" style=""{phho.Location()}""/>",
             Groodle => $@"<img src = "".\img\groodle.png"" style=""{phho.Location()}""/>",
             _ => $@"<div style=""{phho.Location()} {phho.Color()}"">{phho.Hho.ConsoleDisplayString}</div>",
+            //_ => $@"<div style=""{phho.Location()} {phho.Color()}"">&</div>",
         };
         return new RenderFragment(b => b.AddMarkupContent(0, html));
     }
 
-    private static string Location(this IConsoleDisplayable hho)
-        => $"position:absolute; left:{hho.Location.Col * WIDTH}px; top:{hho.Location.Row * HEIGHT}px; width:{WIDTH}px; height:{HEIGHT}px;";
+    //private static string Location(this IConsoleDisplayable hho)
+    //    => $"position:absolute; left:{hho.Location.Col * WIDTH}px; top:{hho.Location.Row * HEIGHT}px; width:{WIDTH}px; height:{HEIGHT}px;";
 
     private static string Location(this PlannedHho phho)
     => $"position:absolute; left:{phho.Col * WIDTH}px; top:{phho.Row * HEIGHT}px; width:{WIDTH}px; height:{HEIGHT}px;";
 
 
-    private static string Color(this IConsoleDisplayable hho) 
-        => $"color:{hho.Color.ToHtmlColor()}; background-color:{hho.BackgroundColor.ToHtmlColor()};";
+    //private static string Color(this IConsoleDisplayable hho) 
+    //    => $"color:{hho.Color.ToHtmlColor()}; background-color:{hho.BackgroundColor.ToHtmlColor()};";
 
     private static string Color(this PlannedHho hho)
     => $"color:{hho.Hho.Color.ToHtmlColor()}; background-color:{hho.Hho.BackgroundColor.ToHtmlColor()};";
