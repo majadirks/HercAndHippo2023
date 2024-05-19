3de117a17ef96e3e14323f867dcbff234f8f79e4
Matthew Dirks <majadirks@gmail.com>
2024-05-18 23:21:51 -0700

    Add misc. graphics (#47)
    Add graphics to Blazor frontend

--- BlazingHippo/DataStructures/Constants.cs
+++ BlazingHippo/DataStructures/Constants.cs
@@ -4,6 +4,8 @@ internal class Constants
     public const int VIEW_MARGIN = 4;
     public const int MIN_DISPLAY_COL = 2;
     public const int MIN_DISPLAY_ROW = 2;
-    public const int GAME_WIDTH = 80;
-    public const int GAME_HEIGHT = 30;
+    public const int GAME_WIDTH = 40;
+    public const int GAME_HEIGHT = 15;
+    public const int IMG_WIDTH = 40;
+    public const int IMG_HEIGHT = 64;
 }
--- BlazingHippo/DataStructures/ImageMapper.cs
+++ BlazingHippo/DataStructures/ImageMapper.cs
@@ -1,32 +1,51 @@
 ﻿using HercAndHippoConsole;
 using HercAndHippoLibCs;
 using Microsoft.AspNetCore.Components;
+using static BlazingHippo.Constants;
 namespace BlazingHippo;
 
 public static class ImageMapper
 {
-    public const int WIDTH = 16;
-    public const int HEIGHT = 32;
+
     public static IEnumerable<IConsoleDisplayable> GetDisplayables(this Level level)
         => level.LevelObjects.Where(obj => obj is IConsoleDisplayable).Cast<IConsoleDisplayable>();
     public static RenderFragment GetHtml(this PlannedHho phho)
     {
         if (phho == null || phho.Hho == null)
             throw new Exception("Unexpected null");
-
+        Color color = phho.Hho.Color;
+        Color bg = phho.Hho.BackgroundColor;
         string html = phho.Hho switch
         {
-            Hippo => $@"<img src = "".\img\hippo.jpeg"" style=""{phho.Location()}""/>",
-            Player => $@"<img src = "".\img\herc.jpeg"" style=""{phho.Location()}""/>",
-            Groodle => $@"<img src = "".\img\groodle.png"" style=""{phho.Location()}""/>",
-            _ => $@"<div style=""{phho.Location()} {phho.Color()}"">{phho.Hho.ConsoleDisplayString}</div>",
-        };
-        return new RenderFragment(b => b.AddMarkupContent(0, html));
-    }
-    private static string Location(this PlannedHho phho)
-    => $"position:absolute; left:{phho.Col * WIDTH}px; top:{phho.Row * HEIGHT}px; width:{WIDTH}px; height:{HEIGHT}px;";
+            Hippo => img("hippo.png"),
+            Player => img("herc.png"),
+            Groodle => img("groodle.png"),
+            Key =>
+                color == Color.DarkMagenta ? img("purple_key.png") :
+                NoImageFile(),
+            BreakableWall => img("lattice.png"),
+            Wall => img("stone_wall.png"),
+            Door =>
+                bg == Color.DarkMagenta ? img("purple_door.png") : NoImageFile(),
+            Ammo => img("quiver.png"),
+            Gem =>
+                color == Color.Red ? img("red_gem.png") : NoImageFile(),
+            Trophy => img("trophy.png"),
+            Bullet => img("arrow.png"),
+            _ => NoImageFile(),
 
-    private static string Color(this PlannedHho hho)
+        }; 
+        return new RenderFragment(b => b.AddMarkupContent(0, html));
+
+        string img(string file)
+            => $@"<img src = "".\img\{file}"" style=""{phho.LocationStyle()}""/>";
+        string NoImageFile()
+        => $@"<div style=""{phho.LocationStyle()} {phho.ColorStyle()}"">{phho.Hho.ConsoleDisplayString}</div>";
+    }
+    private static string LocationStyle(this PlannedHho phho)
+    => $"position:absolute; left:{phho.Col * IMG_WIDTH}px; top:{phho.Row * IMG_HEIGHT}px; width:{IMG_WIDTH}px; height:{IMG_HEIGHT}px;";
+
+    private static string ColorStyle(this PlannedHho hho)
     => $"color:{hho.Hho.Color.ToHtmlColor()}; background-color:{hho.Hho.BackgroundColor.ToHtmlColor()};";
 
 }
